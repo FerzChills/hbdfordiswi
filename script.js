810b6b5d2ebf8b81c9dd5e2191f90cbe47e951e3
FerzChills <106308916+FerzChills@users.noreply.github.com>
2023-12-26 19:11:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector(".button");
+const message = "Selamat ulang tahun, dipeliling keluarga besar";
+
+button.addEventListener("click", function () {
+  alert(message);
+});
